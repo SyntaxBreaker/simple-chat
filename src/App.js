c83090fb7fcb1db61148a45311b9ae85dbf3e6b9
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-05-05 14:07:38 +0200

    Change page title and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/global.scss';
 import UserProvider from './providers/UserProvider';
 import Chat from './components/Chat';
 
 
 function App() {
+  useEffect(() => {
+    document.title = 'Simple chat'
+  }, [])
+
   return (
     <UserProvider>
       <div className="App">
